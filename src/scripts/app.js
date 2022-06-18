5153f831fa1fafe111d0bd3b2705fa7108f6b1b8
Kate Scherz <kate.scherz@gladeye.com>
2022-06-18 15:48:25 +1200

    Add success page

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -9,14 +9,22 @@ class App {
    * Create app.
    */
   constructor() {
+    this.overviewPage = document.querySelector('.overview-page');
+    this.successPage = document.querySelector('.success-page');
+    this.showCorrectPage();
+
     const initialBookmarks = [{name: 'Google', url: 'https://www.google.com'}]; // TODO Replace with persisted links
     this.bookmarks = new Set();
     this.bookmarksDisplayElement = document.querySelector('.bookmarks');
     this.form = new Form(document.querySelector('.add-bookmark-form'));
     this.form.onSuccess = (data) => {
-      this.createNewBookmark(data.get('name'), data.get('url'));
+      const name = data.get('name');
+      const url = data.get('url');
+      this.createNewBookmark(name, url);
       this.form.reset();
-      // TODO go to submit success page
+      this.successPage.querySelector('.success-bookmark-name').innerHTML = name;
+      this.successPage.querySelector('.success-bookmark-url').innerHTML = url;
+      this.goToSuccessPage();
     };
 
     for (const {name, url} of initialBookmarks) {
@@ -34,6 +42,36 @@ class App {
     this.bookmarks.add(newBookmark);
     newBookmark.appendTo(this.bookmarksDisplayElement);
     newBookmark.onRemove(() => this.bookmarks.delete(newBookmark));
+  };
+
+  /**
+   * Show correct page.
+   */
+  showCorrectPage = () => {
+    if (window.location.pathname === '/#success') {
+      this.goToSuccessPage();
+    } else {
+      this.goToOverviewPage();
+    }
+  };
+
+  /**
+   * Go to success page.
+   */
+  goToSuccessPage = () => {
+    window.history.pushState({}, '', '/#success');
+    this.overviewPage.classList.add('hide');
+    this.successPage.classList.remove('hide');
+  };
+
+  /**
+   * Go to overview page.
+   */
+  goToOverviewPage = () => {
+    console.log('going to overviw', this.overviewPage, this.successPage);
+    window.history.pushState({}, '', '/');
+    this.overviewPage.classList.remove('hide');
+    this.successPage.classList.add('hide');
   };
 }
 
