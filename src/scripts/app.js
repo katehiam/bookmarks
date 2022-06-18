12972d692927b4d7ce1c90d2fe51376b50e815c4
Kate Scherz <kate.scherz@gladeye.com>
2022-06-18 13:06:58 +1200

    Add bookmark class and add Edit/Delete buttons

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,3 +1,6 @@
+import {Bookmark} from '@scripts/bookmark';
+import {Form} from '@scripts/form';
+
 /**
  * Entry point.
  */
@@ -6,19 +9,18 @@ class App {
    * Create app.
    */
   constructor() {
-    this.bookmarks = [{name: 'Test', url: 'https://google.com'}];
+    const initialBookmarks = [{name: 'Google', url: 'https://www.google.com'}];
+    this.bookmarks = [];
     this.bookmarksDisplayElement = document.querySelector('.bookmarks');
+    this.form = new Form(document.querySelector('.add-bookmark-form'));
 
-    for (const {name, url} of this.bookmarks) {
-      const newBookmarkLink = document.createElement('a');
-      newBookmarkLink.innerHTML = name;
-      newBookmarkLink.href = url;
-      newBookmarkLink.target = '_blank';
-
-      const newBookmarkListItem = document.createElement('li');
-      newBookmarkListItem.appendChild(newBookmarkLink);
-
-      this.bookmarksDisplayElement.appendChild(newBookmarkListItem);
+    for (const {name, url} of initialBookmarks) {
+      const newBookmark = new Bookmark(name, url);
+      this.bookmarks.push(newBookmark);
+      if (newBookmark.active) {
+        const newBookmarkListItem = newBookmark.generateBookmarkListItem();
+        this.bookmarksDisplayElement.appendChild(newBookmarkListItem);
+      }
     }
   }
 }
---
+++ src/scripts/bookmark.js
@@ -0,0 +1,90 @@
+/**
+ * Bookmark.
+ */
+class Bookmark {
+  /**
+   * Initialise form interactions.
+   * @param {string} name Custom name for bookmark
+   * @param {string} url URL of website to bookmark
+   */
+  constructor(name, url) {
+    this.name = name;
+    this.url = url;
+    this.identifier = this.generateClassName();
+    this.active = true;
+  }
+
+  /**
+   * Generate random classname (avoid conflict between bookmarks with same name)
+   * @param {number} length Length of random string to be generated
+   * @return {string}
+   */
+  generateClassName = (length = 10) => {
+    let result = '';
+    const characters =
+      'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    const charactersLength = characters.length;
+    for (let i = 0; i < length; i++) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+  };
+
+  /**
+   * Generate bookmark list item DOM element.
+   * @return {HTMLElement}
+   */
+  generateBookmarkListItem = () => {
+    const bookmarkLink = document.createElement('a');
+    bookmarkLink.innerHTML = this.name;
+    bookmarkLink.href = this.url;
+    bookmarkLink.target = '_blank';
+
+    const bookmarkDeleteButton = document.createElement('button');
+    bookmarkDeleteButton.innerHTML = 'Delete';
+    bookmarkDeleteButton.addEventListener('click', this.handleDeleteClick);
+
+    const bookmarkEditButton = document.createElement('button');
+    bookmarkEditButton.innerHTML = 'Edit';
+    bookmarkEditButton.addEventListener('click', this.handleEditClick);
+
+    const bookmarkListItem = document.createElement('li');
+    bookmarkListItem.classList.add(this.identifier);
+    bookmarkListItem.appendChild(bookmarkLink);
+    bookmarkListItem.appendChild(bookmarkEditButton);
+    bookmarkListItem.appendChild(bookmarkDeleteButton);
+
+    return bookmarkListItem;
+  };
+
+  /**
+   * Handle "delete" button click.
+   */
+  handleDeleteClick = () => {
+    this.remove();
+  };
+
+  /**
+   * Handle "edit" button click.
+   */
+  handleEditClick = () => {
+    this.showEditPanel();
+  };
+
+  /**
+   * Show edit form for bookmark.
+   */
+  showEditPanel = () => {
+    // TODO
+  };
+
+  /**
+   * Remove relative DOM element.
+   */
+  remove = () => {
+    this.active = false;
+    document.querySelector(`.${this.identifier}`).remove();
+  };
+}
+
+export {Bookmark};
