642240b37eeb2582b6fd93025d72ea54a8eb3aff
Kate Scherz <kate.scherz@gladeye.com>
2022-06-18 12:02:53 +1200

    Output test bookmark to page

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -5,7 +5,21 @@ class App {
   /**
    * Create app.
    */
-  constructor() {}
+  constructor() {
+    this.bookmarks = [{name: 'Test', url: 'https://google.com'}];
+    this.bookmarksDisplayElement = document.querySelector('.bookmarks');
+
+    for (const {name, url} of this.bookmarks) {
+      const newBookmarkLink = document.createElement('a');
+      newBookmarkLink.innerHTML = name;
+      newBookmarkLink.href = url;
+
+      const newBookmarkListItem = document.createElement('li');
+      newBookmarkListItem.appendChild(newBookmarkLink);
+
+      this.bookmarksDisplayElement.appendChild(newBookmarkListItem);
+    }
+  }
 }
 
 export {App};
