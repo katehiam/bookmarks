a54604e9d419d54cea3a637a3aeb52f7891656a5
Kate Scherz <kate.scherz@gladeye.com>
2022-06-18 16:27:58 +1200

    Add basic pagination

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,6 +1,8 @@
 import {Bookmark} from '@scripts/bookmark';
 import {Form} from '@scripts/form';
 
+const BOOKMARKS_PER_PAGE = 2;
+
 /**
  * Entry point.
  */
@@ -9,13 +11,32 @@ class App {
    * Create app.
    */
   constructor() {
+    // Pages
     this.overviewPage = document.querySelector('.overview-page');
     this.successPage = document.querySelector('.success-page');
     this.showCorrectPage();
 
-    const initialBookmarks = [{name: 'Google', url: 'https://www.google.com'}]; // TODO Replace with persisted links
+    // Bookmarks
+    const initialBookmarks = [
+      {name: 'Google', url: 'https://www.google.com'},
+      {name: 'Google', url: 'https://www.google.com'},
+      {name: 'Google', url: 'https://www.google.com'},
+    ]; // TODO Replace with persisted links
     this.bookmarks = new Set();
+
     this.bookmarksDisplayElement = document.querySelector('.bookmarks');
+
+    for (const {name, url} of initialBookmarks) {
+      this.createNewBookmark(name, url);
+    }
+
+    this.currentPageNumber = 1;
+    this.numberPages = Math.ceil(this.bookmarks.size / BOOKMARKS_PER_PAGE);
+    this.pagination = document.querySelector('.pagination');
+    this.makePagination();
+    this.addBookmarksToPage();
+
+    // Forms
     this.form = new Form(document.querySelector('.add-bookmark-form'));
     this.form.onSuccess = (data) => {
       const name = data.get('name');
@@ -28,10 +49,6 @@ class App {
       this.successPage.querySelector('.success-bookmark-url').innerHTML = url;
       this.goToSuccessPage();
     };
-
-    for (const {name, url} of initialBookmarks) {
-      this.createNewBookmark(name, url);
-    }
   }
 
   /**
@@ -42,8 +59,87 @@ class App {
   createNewBookmark = (name, url) => {
     const newBookmark = new Bookmark(name, url);
     this.bookmarks.add(newBookmark);
-    newBookmark.appendTo(this.bookmarksDisplayElement);
     newBookmark.onRemove(() => this.bookmarks.delete(newBookmark));
+  };
+
+  /**
+   * Create pagination DOM.
+   */
+  makePagination = () => {
+    if (!this.numberPages || this.numberPages === 1) return;
+
+    if (this.currentPageNumber > 1) {
+      const pageLinkPrevious = document.createElement('a');
+      pageLinkPrevious.innerHTML = 'Previous';
+      pageLinkPrevious.href =
+        this.currentPageNumber - 1 === 1
+          ? '/'
+          : `/?page=${this.currentPageNumber - 1}`;
+      pageLinkPrevious.addEventListener('click', (e) => {
+        e.preventDefault();
+        this.navigateToPage(this.currentPageNumber - 1);
+      });
+      this.pagination.appendChild(pageLinkPrevious);
+    }
+
+    for (let i = 0; i < this.numberPages; i++) {
+      // append pagination
+      const pageLink = document.createElement('a');
+      pageLink.innerHTML = i + 1;
+      pageLink.href = i === 0 ? '/' : `/?page=${i + 1}`;
+      pageLink.addEventListener('click', (e) => {
+        e.preventDefault();
+        this.navigateToPage(i + 1);
+      });
+      this.pagination.appendChild(pageLink);
+    }
+
+    if (this.currentPageNumber < this.numberPages) {
+      const pageLinkNext = document.createElement('a');
+      pageLinkNext.innerHTML = 'Next';
+      pageLinkNext.href = `/?page=${this.currentPageNumber + 1}`;
+      pageLinkNext.addEventListener('click', (e) => {
+        e.preventDefault();
+        this.navigateToPage(this.currentPageNumber + 1);
+      });
+      this.pagination.appendChild(pageLinkNext);
+    }
+  };
+
+  /**
+   * Add bookmarks to page with pagination.
+   */
+  addBookmarksToPage = () => {
+    const bookmarksOnThisPage = Array.from(this.bookmarks).slice(
+      (this.currentPageNumber - 1) * BOOKMARKS_PER_PAGE,
+      this.currentPageNumber * BOOKMARKS_PER_PAGE,
+    );
+    for (const bookmark of bookmarksOnThisPage) {
+      this.addBookmarkToPage(bookmark);
+    }
+  };
+
+  /**
+   * Add bookmark to page.
+   * @param {Bookmark} bookmark Bookmark to be added to page
+   */
+  addBookmarkToPage = (bookmark) => {
+    bookmark.appendTo(this.bookmarksDisplayElement);
+  };
+
+  /**
+   * Navigate to page.
+   * @param {number} pageNumber Page number to navigate to
+   */
+  navigateToPage = (pageNumber) => {
+    this.currentPageNumber = Math.max(
+      1,
+      Math.min(pageNumber, this.numberPages),
+    );
+    this.bookmarksDisplayElement.innerHTML = '';
+    this.pagination.innerHTML = '';
+    this.makePagination();
+    this.addBookmarksToPage();
   };
 
   /**
@@ -70,7 +166,6 @@ class App {
    * Go to overview page.
    */
   goToOverviewPage = () => {
-    console.log('going to overviw', this.overviewPage, this.successPage);
     window.history.pushState({}, '', '/');
     this.overviewPage.classList.remove('hide');
     this.successPage.classList.add('hide');
